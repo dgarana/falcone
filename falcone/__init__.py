74de4b9b0fd9861df3ec1a8be34b0e08cd9de111
David Garaña <david.garana@7ideas.com>
2016-06-23 12:21:48 +0200

    Prepare package
    First commit with basic skeleton to prepare the package itself.
    The package contains: An installation setup.py, that handles with
    requirements files, a tox configured to check flake8 and execute
    pytest against `tests` folder.

---
+++ falcone/__init__.py
@@ -0,0 +1,13 @@
+#! python
+# -* coding: utf-8 *-
+"""
+Package Falcone v0.0.0 alpha
+"""
+# System imports
+import time
+# Third-party imports
+# Local imports
+
+
+# Following PEP386
+__version__ = (0, 0, 0, 'alpha', int(time.time()))
---
+++ tests/sample_test.py
@@ -0,0 +1,12 @@
+#!python
+# -* coding: utf-8 *-
+"""
+Dummy test sample
+"""
+# System imports
+# Third-party imports
+# Local imports
+
+
+def test_true():
+    assert True
