27bd3f13616bd53f1b291bf00b3370943289b5b7
David Garaña <david.garana@7ideas.com>
2016-06-23 17:40:05 +0200

    Experimental

---
+++ falcone/middleware/__init__.py
@@ -0,0 +1,13 @@
+#!python
+# -* coding: utf-8 *-
+"""
+falcone.middleware module
+"""
+# System imports
+# Third-party imports
+# Local imports
+from .classes import JSONTranslator
+from .classes import RequireJSON
+
+
+__all__ = ['JSONTranslator', 'RequireJSON']
---
+++ falcone/middleware/classes.py
@@ -0,0 +1,53 @@
+#!python
+# -* coding: utf-8 *-
+"""
+Middleware classes
+"""
+# System imports
+import falcon
+import json
+# Third-party imports
+# Local imports
+
+
+class RequireJSON(object):
+
+    def process_request(self, req, resp):
+        if not req.client_accepts_json:
+            raise falcon.HTTPNotAcceptable(
+                'This API only supports responses encoded as JSON.',
+                href='http://docs.examples.com/api/json')
+
+        if req.method in ('POST', 'PUT'):
+            if 'application/json' not in req.content_type:
+                raise falcon.HTTPUnsupportedMediaType(
+                    'This API only supports requests encoded as JSON.',
+                    href='http://docs.examples.com/api/json')
+
+
+class JSONTranslator(object):
+
+    def process_request(self, req, resp):
+        if req.content_length in (None, 0):
+            return
+
+        body = req.stream.read()
+        if not body:
+            raise falcon.HTTPBadRequest('Empty request body',
+                                        'A valid JSON document is required.')
+
+        try:
+            req.context['doc'] = json.loads(body.decode('utf-8'))
+
+        except (ValueError, UnicodeDecodeError):
+            raise falcon.HTTPError(falcon.HTTP_753,
+                                   'Malformed JSON',
+                                   'Could not decode the request body. The '
+                                   'JSON was incorrect or not encoded as '
+                                   'UTF-8.')
+
+    def process_response(self, req, resp, resource):
+        if 'result' not in req.context:
+            return
+
+        resp.body = json.dumps(req.context['result'])
