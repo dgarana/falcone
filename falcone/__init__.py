cf7f13d68c70f9942f27a798640c6baecec74fb0
David Garaña <dgarana@live.com>
2019-02-18 21:38:52 +0100

    Fix setup.py file
    Remove the old way of installing requirements on setup.py file.

--- falcone/__init__.py
+++ falcone/__init__.py
@@ -4,10 +4,7 @@
 Package Falcone v0.0.0 alpha
 """
 # System imports
-import time
 # Third-party imports
 # Local imports
 
-
-# Following PEP386
-__version__ = (0, 0, 0, 'alpha', int(time.time()))
+__version__ = '0.0.0.alpha'
